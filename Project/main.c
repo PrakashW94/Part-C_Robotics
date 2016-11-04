cf6a7775437df135697f21452b6ccc044e5564cc
Kiblinix <JackAMoore94@gmail.com>
2016-11-04 10:46:34 +0000

    Moves for 1000 steps and stops
    With common wait function

--- Project/main.c
+++ Project/main.c
@@ -5,7 +5,28 @@
 int main(void)
 {
 	e_init_motors();
-	e_set_speed_left(500); //go forward on half speed
-	e_set_speed_right(-500); //go backward on half speed
-	while(1) {}
+	
+	e_set_speed_left(500);
+	e_set_speed_right(500);	
+	
+	while(1) 
+	{
+		if (e_get_steps_left() > 1000)
+		{
+			e_set_speed_left(0);
+			e_set_speed_right(0);
+		}
+
+		Wait(15000);
+	}
+}
+
+void Wait(int steps)
+{
+	int i = 0;
+
+	for(i=0; i<steps; i++)
+	{
+ 		asm("NOP");
+	}
 }
