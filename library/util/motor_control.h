10121fc4266bf9f5351cd27e87be025bcad38c9d
Kiblinix <JackAMoore94@gmail.com>
2016-11-16 13:27:44 +0000

    Revert "Merge pull request #10 from PrakashW94/JM-PW"
    This reverts commit 0853f7192ce74d30c97e7a939f5d82eb3f6c7c52, reversing
    changes made to 941fe616622291c1ac34a68257839f97135b4b21.

--- library/util/motor_control.h
+++ library/util/motor_control.h
@@ -2,7 +2,6 @@ void waitForSteps(int steps);
 void clearSteps();
 void rotateClockwiseDegrees( int degrees );
 void rotateClockwise( int steps );
-void rotateAntiClockwiseDegrees( int degrees )
 void rotateAntiClockwise(int steps);
 void moveForwards( int steps );
 void moveToPoint(int stepsRight, int stepsForward);
