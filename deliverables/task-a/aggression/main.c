9689020e2899eeaec8049e8e9c9f43513a14d59d
Matthew Turner <matthewturner@eduroam-student-128-43.lut.ac.uk>
2016-11-17 13:38:12 +0000

    Added project files. Updated main

--- deliverables/task-a/aggression/main.c
+++ deliverables/task-a/aggression/main.c
@@ -1,6 +1,11 @@
 #include "behaviour/aggression.h"
-#include "motor_led/e_epuck_ports.h"
 
+#include "motor_led/e_epuck_ports.h"
+#include "motor_led/e_motors.h"
+#include "a_d/e_prox.h"
+
+#include "custom_util/motor_control.h"
+#include "custom_util/constants.c"
 
 int getSelector() 
 {
@@ -8,21 +13,22 @@ int getSelector()
 }
 
 int main()
-{
-	e_init_port();
-	e_init_uart1();	
-	e_init_prox();
-	
+{	
 	int selector = getSelector();
-
+	
+	e_init_port();
+	
+	// Selectors go from 0-14
 	switch( selector ){
 		case 0:
 			initAggression();
+			break;
 		default:
 			BODY_LED = 1;
 			while(1);
 			break;
 	} 
+	
 
 	return 0;	
 }
