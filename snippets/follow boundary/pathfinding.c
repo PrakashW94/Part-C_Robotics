9ca466e26f0674abd681ba6a5abfce374d2162fd
PrakashW94 <cgpw@lunet.lboro.ac.uk>
2016-12-01 15:50:03 +0000

    Some progress
    Updated tracking to be local and Jack's fixes for angle calcs

--- snippets/follow boundary/pathfinding.c
+++ snippets/follow boundary/pathfinding.c
@@ -37,14 +37,17 @@ x: current x value
 y: current y value
 xg: x goal value
 yg: y goal value
-r: number of steps rotated from horiz 
+r: number of steps rotated in segment of movement
+h: number of steps moved linearly in a segment of movement
+rCurrent: total number of steps rotated
 q[]: running counter for percentage complete
 d[]: running counter for distance from m-line
 i: counter for q and d
 j: program steps counter
+fp: front(wide) prox value
 */
 
-int x, y, xg, yg, r, i, j, fp;
+int x, y, xg, yg, r, h, rCurrent, i, j, fp;
 int q[100];
 double d[100];
 
@@ -74,15 +77,24 @@ int stepsToRotate(double angle)
 
 double angleFromGoal(double x, double y, double xg, double yg, int stepsRotated)
 {
-    double alpha;
-    alpha  = atan((xg-x)/(yg-y));
-	if (xg - x < 0 && yg - y < 0)
-	{
-		alpha += PI;
+	double diffX = xg-x;
+	double diffY = yg-y;
+    double a = pow(diffX, 2);
+	double b = pow(diffY, 2);
+	double linDist = sqrt(a + b);
+	double angle = acos(diffY / linDist);
+
+	if (diffX < 0)
+	{	
+		angle = (2 * PI) - angle;
 	}
-    double theta;
-    theta = angleToRotate(stepsRotated);
-    return alpha - theta;
+	// Take into account how we're currently oriented.
+	angle -= angleToRotate(stepsRotated);;
+	if (angle < 0)
+	{
+		angle = 2*PI + angle;
+	}
+	return angle;
 }
 
 double distToMline(double x, double y, double xg, double yg)
@@ -120,6 +132,9 @@ void setGoal()
 	xg = 0;
 	yg = 5000;
 	fp = 0;
+	h = 0;
+	r = 0;
+	rCurrent = 0;
 	
 	i = 1;
 	j = 0;
@@ -136,52 +151,34 @@ void setSpeed(int l, int r)
 
 void updateProgress()
 {
-	int h = e_get_steps_left();
+	rCurrent += r;
 	double angle = angleToRotate(r);
-	if (angle < 0)
-	{
-		angle += PI;
-		reportValue("Added PI to angle", RADtoDEG(angle));
-	}
-	if (r == 0)
-	{
-		x = floor(h * sin(angle));
-		y = floor(h * cos(angle));
-	}
-	else
-	{
-		
-		x = floor(h * cos(angle));
-		y = floor(h * sin(angle));
-	}
+
+	x += floor(h * sin(angle));
+	y += floor(h * cos(angle));
+	
 	d[i] = distToGoal(x, y, xg, yg);
 	q[i] = 100 - floor((d[i]/d[0])*100);
 	j++;
+	clearSteps();
 }
 
 void moveToGoal()
 {
-	double angle = angleFromGoal(x, y, xg, yg, r);
+	double angle = angleFromGoal(x, y, xg, yg, rCurrent);
 	
-	if (angle < 0)
+	if (angle > PI)
 	{
-		angle *= -1; 
-		r = stepsToRotate(angle);
-		int h = e_get_steps_left();
+		angle = 2*PI - angle; 
 		setSpeed(-s, s);
-		waitForSteps(r);
-		e_set_steps_left(h);
-		setSpeed(s, s);
 	}
 	else
 	{
-		r = stepsToRotate(angle);
-		int h = e_get_steps_left();
 		setSpeed(s, -s);
-		waitForSteps(r);
-		e_set_steps_left(h);
-		setSpeed(s, s);
 	}
+	r = stepsToRotate(angle);
+	waitForSteps(r);
+	setSpeed(s, s);
 }
 
 int getProx(int sensors[], int noOfSensors)
@@ -206,7 +203,8 @@ void progressReport()
 	reportValue("Steps h", e_get_steps_left());
 	reportValue("Distance from Goal", d[i]);
 	reportValue("Percentage Complete", q[i]);
-	reportValue("Steps rotated", r);
+	reportValue("Current Direction (steps)", (rCurrent));
+	reportValue("Current Direction", RADtoDEG(angleToRotate(rCurrent)));
 	reportValue("Number of states", j);
 	reportValue("****END PROGRESS REPORT****", -1);
 }
@@ -217,86 +215,71 @@ int avoidBoundary()
 	//int rightLeds[] = {1, 2, 3};
 	//int leftLeds[] = {5, 6, 7};
 	
-	//int frontProx = getProx(front, 2);
+	//reportValue("Avoiding object IN", -1);
+	int mDist;
+	
+	clearSteps();
+	r = 0;
+
 	int frontProx = (int)((e_get_prox(7) + e_get_prox(0))/2);
-	
-	int r0, rStore, h;
-	r0 = 0;
-	
-	int mDist = 0;
-	
-	//left led on
-	//setLeds(leftLeds, 3);
-	rStore = r;
-	r0 = e_get_steps_right();
-	h = e_get_steps_left();
-	while (frontProx > 150)
+	while (frontProx > 400)
 	{
 		//turn left until front prox doesn't detect object
-		//reportValue("turning left", frontProx);
 		setSpeed(-s, s);
 		frontProx = (int)((e_get_prox(7) + e_get_prox(0))/2);
 		wait(delayTimer);
 	}
-	//left led off
-	//setLeds(leftLeds, 3);
-	r -= e_get_steps_right() - r0;
-	e_set_steps_left(h);
+
+	r += e_get_steps_left();
+	reportValue("rotated", RADtoDEG(angleToRotate(r)));
 	
 	int rightProx = (int)((e_get_prox(0) + e_get_prox(1) + e_get_prox(2))/3);
-	//wait(delayTimer); //wait to get correct prox value
 	int leftProx =  (int)((e_get_prox(0) + e_get_prox(7) + e_get_prox(6) + e_get_prox(5))/4);
-	//wait(delayTimer); //wait to get correct prox value
-	while (rightProx > 100)
+	while (rightProx > 300)
 	{
-		//go straight, if right senses object, turn left a bit
 		switch(rightProx)
 		{
-			case 100 ... 650: 
-			{//go straight, increasing right
-				//setLeds(frontLeds, 1);
-				while ((rightProx < 650) && (rightProx > 100))
+			case 300 ... 650: 
+			{//go straight with the object on the right
+				while ((rightProx < 650) && (rightProx > 300))
 				{
-					if(leftProx > 400)
-					{
+					if(leftProx > 300)
+					{//detect object on left, loop function to avoid
 						reportValue("entering recursion loop", leftProx);
 						avoidBoundary();
 					}
 					else
-					{
-						//reportValue("going straight, object on right", rightProx);
+					{//go straight and check for mline
 						setSpeed(s, s);
+						h = e_get_steps_left();
 						updateProgress();
 						mDist = distToMline(x, y, xg, yg);
-						reportXY(x, y, mDist);
-						if (mDist < 50)
-						{
-							reportValue("ON MLINE, returning to main code", -1);
-							return 1;
-						}
+						//reportXY(x, y, mDist);
+						//if (mDist < 50)
+						//{
+							//reportValue("ON MLINE, returning to main code", -1);
+							//return 1;
+						//}
 					}
+					//reportValue("linear movement", h);
 					rightProx = (int)((e_get_prox(0) + e_get_prox(1) + e_get_prox(2))/3);
 					leftProx = (int)((e_get_prox(0) + e_get_prox(7) + e_get_prox(6) + e_get_prox(5))/4);
 					wait(delayTimer);
 				}
-				//setLeds(frontLeds, 1);
 				break;
 			}
 			case 651 ... 2000:
-			{//object found, turn left, decreasing right
-				//setLeds(leftLeds, 3);
-				r0 = e_get_steps_right();
-				h = e_get_steps_left();
+			{//moving towards object, turn left slightly
+				clearSteps();
 				while (rightProx > 650)
 				{
-					//reportValue("turning left a bit", rightProx);
 					setSpeed(-s, s);
 					rightProx = (int)((e_get_prox(0) + e_get_prox(1) + e_get_prox(2))/3);
 					wait(delayTimer);
 				}
-				//setLeds(leftLeds, 3);
-				r -= e_get_steps_right() - r0;
-				e_set_steps_left(h);
+				r += e_get_steps_left();
+				reportValue("rotated", RADtoDEG(angleToRotate(r)));
+				e_set_steps_left(0);
 				break;
 			}
 			default: 
@@ -307,47 +290,38 @@ int avoidBoundary()
 		}
 		rightProx = (int)((e_get_prox(0) + e_get_prox(1) + e_get_prox(2))/3);
 	}
-
-	//setLeds(rightLeds, 3);
-	r0 = e_get_steps_left();
-	reportValue("r0", r0);
-	h = e_get_steps_left();
-	//this need to be initialised as 0 as first value is incorrect.
+	
+	clearSteps();
 	leftProx = (int)((e_get_prox(7) + e_get_prox(0) + e_get_prox(1))/3);
-	while (leftProx < 80)
-	{
-		//turn right until left senses object
-		//reportValue("turning right", leftProx);
+	while (leftProx < 150)
+	{//turn right until object is on the right
 		setSpeed(s, -s);
 		leftProx = (int)((e_get_prox(7) + e_get_prox(0) + e_get_prox(1))/3);
-		//reportValue("left steps", e_get_steps_left());
 		wait(delayTimer);
 	}
-	//left leds off
-	//setLeds(rightLeds, 3);
-	reportValue("left steps", e_get_steps_left());
-	r += e_get_steps_left() - r0;
-	e_set_steps_left(h);
-	reportValue("change in r", r - rStore);
-	reportValue("lin distance travelled", h);
+	r += e_get_steps_left();
+	wait(delayTimer);
 	setSpeed(s, s);
+	progressReport();
 	return 0;
 }
 
 void pathfinder()
-{	
+{
 	setGoal();
 	while(1)
 	{
 		moveToGoal();
-		while (fp < 150 && q[i] < 95)
+		e_set_steps_left(0);
+		while (fp < 300 && q[i] < 95)
 		{
 			//fp = getProx(frontwide, 4);
 			fp = (int)((e_get_prox(6) + e_get_prox(7) + e_get_prox(0) + e_get_prox(1))/4);
-			waitForSteps(50); //wait for time?
+			waitForSteps(50);
+			h = e_get_steps_left();
 			updateProgress();
 			//reportValue("Frontwide", fp);
-			//reportValue("q[i]", q[i]);
+			reportValue("q[i]", q[i]);
 			//progressReport();
 		}
 		if (q[i] > 95)
@@ -366,25 +340,20 @@ void pathfinder()
 		do 
 		{
 			onMline = avoidBoundary();
-/*			
-			updateProgress();
-			mDist = distToMline(x, y, xg, yg);
-			reportValue("Distance to M-line", mDist);
-*/
-			progressReport();
 			fp = (int)((e_get_prox(6) + e_get_prox(7) + e_get_prox(0) + e_get_prox(1))/4);
-			if (onMline)
+			/*if (onMline)
 			{
+				progressReport();
 				reportValue("ON MLINE, Main Prog", -1);
 				reportValue("db", db);
 				reportValue("d[i]", d[i]);
 				reportValue("fp", fp);
-			}
+			}*/
 		} while
 		(
 			q[i] < 95 &&
 			//q[i] == qb &&
-			!(onMline && db > d[i] && fp < 200)
+			!(onMline && db > d[i] && fp < 300)
 		);
 		
 		if (q[i] > 95)
